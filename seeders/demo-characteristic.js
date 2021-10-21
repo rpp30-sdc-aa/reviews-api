88f3e47e00186c9457937db668b3c6fd739bf895
Alexander Olvera <alexander.olvera943@gmail.com>
2021-10-21 16:57:04 -0500

    Added seed file for characteristic table

---
+++ seeders/demo-characteristic.js
@@ -0,0 +1,36 @@
+'use strict';
+const path = require('path')
+const { seedDatabaseFromCSV } =  require('./utils/utils.js')
+
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    const transformCallBack = data => {
+      const {id, ...rest} = data;
+      return {
+        id: Number(id),
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+        ...rest
+      }
+    }
+
+    try {
+      let rowsAdded = await seedDatabaseFromCSV(
+        path.resolve(__dirname, 'sample', 'characteristics.csv'),
+        queryInterface,
+        100000,
+        transformCallBack
+      )
+
+      console.log('Database seed complete. Rows added: ', rowsAdded)
+    } catch(err) {
+      console.log(err)
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Reviews', null, {});
+  }
+};
