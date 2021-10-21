94bed0c51ccfa2b840ef1dea1c1f61514caebf4f
Alexander Olvera <alexander.olvera943@gmail.com>
2021-10-21 17:21:34 -0500

    Added initial route for reviews using the new getReviews controller

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
-
 const app = express()
+const { getReviews } = require('../controllers/index.js')
 
-const db = require('../models/index.js');
+
+app.get('/reviews', async (req, res) => {
+  const { page, product_id, limit } = req.query;
+  try {
+    let reviews = await getReviews(product_id, limit, page)
+    res.json({
+      product_id,
+      page,
+      reviews
+    })
+  } catch(err) {
+    console.log(error)
+    res.status(500).send('Failed to fetch reviews...')
+  }
+});
 
 app.listen(3005, () => {console.log('Server started....')})
